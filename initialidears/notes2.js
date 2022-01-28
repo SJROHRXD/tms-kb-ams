ad7611314bb45a057ebb1a9acac7ae5cb336c835
SJRXD <rohr.olasz@gmail.com>
2022-01-27 22:23:50 -0800

    added a second js notes doc w/ some rough notes on kid's task potential and different variations of tasks

---
+++ initialidears/notes2.js
@@ -0,0 +1,43 @@
+// Rough Draft - Task and Other Item Idea Notes
+
+// Daily, Weekly, Monthly, Every-So-Many X, Etc
+// Place for Obligations / Things to Remember
+// Appointments
+// Events
+// And Tasks!
+
+// Daily Record would include
+// Food, Snacks
+// Things I did "Of Note"
+// Chores, Tasks, Admin, Professional
+// Things I remember we need; like groceries or home items
+
+// 🌷🌿🌼 🌷🌿🌼 🌷🌿🌼 🌷🌿🌼 🌷🌿🌼 🌷🌿🌼 🌷🌿🌼 //
+
+// Kid's Planner Notes
+// Mobile App 🌼
+// Printable:
+    // Consider paper weight
+    // Grid, Graph
+    // Spiral / Lay Flat
+    // Space for Doodles
+    // Header
+    // Filled in Monthly Calendar Dates
+
+// A way to insert Parent Schedule
+// Daily:
+    // Spot for hygiene and health
+    // Checklists
+        // Brush Teeth x 2, Floss
+        // Skin Care x 2
+        // Shower Routine, Deoderant
+    // HW or Assignments
+        // Math, Language, Social Studies, Etc
+    // "Remember to Ask Mom/Dad" Section
+        // Bulleted on paper
+        // Field that auto-bullets on app
+    // Space to draw (figure that out on mobile lol)
+    // Useful or motivational quotes - there's gotta be an API
+    // Optional MINDFULLNESS Section
+    // Chores / Tasks Completed
+    // Self Care Section that ties into the other things
